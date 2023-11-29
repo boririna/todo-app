d7c9cdb7b940f4e2b9842c46e205e35bd65bb013
Irina Borisova <i.a.borisova@gmail.com>
2023-11-29 17:55:36 +0100

    added todos form jason.placeholder

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import { createElement } from 'react';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
-export function App() {
-	const currentYear = new Date().getFullYear();
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement('p'),
-			'Edit src/App.js and save to reload.',
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			createElement('p', null, currentYear),
-		),
+export const App = () => {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos')
+			.then((loadedData) => loadedData.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos);
+			});
+	}, []);
+
+	return (
+		<div className={styles.App}>
+			<h1>Todos</h1>
+			{todos.map(({ userId, id, title, completed }) => (
+				<div key={id}>
+					{id}. {title} - {completed ? <span>V</span> : <span>X</span>}
+				</div>
+			))}
+		</div>
 	);
-}
-
-// export const App = () => {
-// 	// declarative style starts
-// 	const currentYear = new Date().getFullYear();
-// 	return (
-// 		<div className="App">
-// 			<header className="App-header">
-// 				<img src={logo} className="App-logo" alt="logo" />
-// 				<p>
-// 					Edit <code>src/App.js</code> and save to reload.
-// 				</p>
-// 				<a
-// 					className="App-link"
-// 					href="https://reactjs.org"
-// 					target="_blank"
-// 					rel="noopener noreferrer"
-// 				>
-// 					Learn React
-// 				</a>
-// 				<p>{currentYear}</p>
-// 			</header>
-// 		</div>
-// 	);
-// };
+};
